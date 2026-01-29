090dce6c8347448af6642176ecebea82bccc9e33
FragosoQ <qualidade@somengil.com>
2026-01-29 15:33:42 +0000

    Adjust progress bars spacing and change globe dots color to darker blue

--- scripts/config.js
+++ scripts/config.js
@@ -16,7 +16,7 @@ const config = {
 		globe: 0.001
 	},
 	colors: {
-		globeDotColor: '#05d5fa',
+		globeDotColor: /*'#05d5fa'*/'#055afa',
 		globeMarkerColor: 'rgb(143, 216, 216)',
 		globeMarkerGlow: 'rgb(255, 255, 255)',
 		globeLines: 'rgb(255, 255, 255)',
